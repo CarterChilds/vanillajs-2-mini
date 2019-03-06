fcc747f976bf97e9f333af5193a02cba98b4b857
CarterChilds <carterchilds@live.com>
2019-03-06 11:25:32 -0700

    started project

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector("form").addEventListener("submit", addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  const item = document.createElement("li");
+  item.innerText = document.getElementById("item").value;
+
+  const list = document.querySelector("ul");
+  list.appendChild(item);
+}
